818ef1d9f6184ac3b03f0c5e379f7a479ca16e07
oiva-terho <oiva.terho@gmail.com>
2022-09-16 14:21:25 +0300

    convert classes to functions with useEffect hook

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,17 +1,12 @@
-import { Component } from "react";
 import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
-export class CardList extends Component {
-
-    render() {
-        const { monsters } = this.props;
-        return <div className='card-list'>
-            {monsters.map(monster => {
-                return (
-                    <Card person={ monster }/>
-                )
-            })}
-        </div>
-    }
-}
+export const CardList = ({ monsters }) => (
+    <div className='card-list'>
+        {monsters.map(monster => {
+            return (
+                <Card monster={ monster }/>
+            )
+        })}
+    </div>
+);
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,10 @@
-import { Component } from "react";
 import './search-box.styles.css';
 
-export class SearchBox extends Component {
-
-    render() {
-        const { onChangeHandler, placeholder, className } = this.props;
-        return(
-            <input 
-            className={ `search-box ${className}` } 
-            type='search' 
-            placeholder={ placeholder } 
-            onChange={ onChangeHandler }
-          />
-        )
-    }
-}
+export const SearchBox = ({ onChangeHandler, placeholder, className }) => (
+    <input 
+        className={ `search-box ${className}` } 
+        type='search' 
+        placeholder={ placeholder } 
+        onChange={ onChangeHandler }
+    />
+);
