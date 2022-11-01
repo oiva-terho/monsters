7f43aeacbd6c4e830c4b51908b27f418cd0ca9df
oiva-terho <oiva.terho@gmail.com>
2022-11-02 00:29:41 +0300

    migrate to TypeScript

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,7 +1,11 @@
 import { Card } from '../card/card.component';
+import { Monster } from '../../App';
 import './card-list.styles.css';
 
-export const CardList = ({ monsters }) => (
+type CardListProp = {
+    monsters: Monster[];
+}
+export const CardList = ({ monsters }: CardListProp) => (
     <div className='card-list'>
         {monsters.map(monster => {
             return (
--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,6 +1,11 @@
+import { Monster } from '../../App';
 import './card.styles.css';
 
-export const Card = ({ monster }) => {
+type CardProps = {
+    monster: Monster;
+}
+
+export const Card = ({ monster }: CardProps) => {
     const { id, name , email } = monster;
     return (
         <div className='card-container' key={ id }>
